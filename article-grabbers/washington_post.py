96269b88eb1637342f25ce24f061c819036111e4
PythonRam9 <86711160+PythonRam9@users.noreply.github.com>
2021-08-14 20:46:45 -0500

    Update washington_post.py

--- article-grabbers/washington_post.py
+++ article-grabbers/washington_post.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-def alaraby(num):
+def wp(num):
     url = 'https://www.washingtonpost.com/world/middle-east/'
 
     response = requests.get(url=url, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'})
