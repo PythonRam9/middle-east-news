ed90eb33a13753cc8f4fe93bcf76665b68e7c4e3
PythonRam9 <86711160+PythonRam9@users.noreply.github.com>
2021-08-14 20:24:57 -0500

    The code for the WP section

---
+++ article-grabbers/washington_post.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+def alaraby(num):
+    url = 'https://www.washingtonpost.com/world/middle-east/'
+
+    response = requests.get(url=url, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'})
+    soup = bs(response.content, 'html.parser')
+
+    soup = (soup.find_all('a', hreflang=True, itemprop=True))
+
+    return [soup[num].text, soup[num]['href']]
+  
